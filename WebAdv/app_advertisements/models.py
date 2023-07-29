3dbf547de05c217dc3331dfb47bf696e26332550
Howrik <kirillvohmin13@gmail.com>
2023-07-29 20:37:17 +0500

    дз 4

--- WebAdv/app_advertisements/models.py
+++ WebAdv/app_advertisements/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Advertisement(models.Model):
-    title = models.CharField("Заголовок", max_length=128)
-    description = models.TextField("Описание")
-    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
-    auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
+    title = models.CharField("заголовок", max_length=128)
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=10, decimal_places=2) # 999.999 max_digits=5, decimal_places=2 
+    auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        return f"Advertisement(id={self.id},title={self.title},price={self.price})"
     class Meta:
         db_table = "advertisements"
-    def __str__(self):
-        return f"Advertisement(id={self.id},title={self.title},price={self.price})"
