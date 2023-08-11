875e19402ccf256ba762685238a5693cd47993b8
Howrik <kirillvohmin13@gmail.com>
2023-08-11 17:29:21 +0500

    дз 4

--- WebAdv/app_advertisements/admin.py
+++ WebAdv/app_advertisements/admin.py
@@ -3,19 +3,20 @@ from .models import Advertisement
 
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction', 'user', 'get_html_image']
     list_filter = ['auction', 'created_at']
     actions = ['make_auction_as_false', 'make_auction_as_true']
     fieldsets = (
         ('Общее', {
-            'fields': ('title', 'description')
+            'fields': ('title', 'description', 'image')
         }),
         ('Финансы', {
             'fields': ('price', 'auction'), 
             'classes': ['collapse']
         })
     )
-
+#  используйте уже знакомые декоратор @admin.display и функцию format_html.
+    
     @admin.action(description='Убрать возможность торга')
     def make_auction_as_false(self, request, queryset):
         queryset.update(auction=False)
--- WebAdv/app_advertisements/models.py
+++ WebAdv/app_advertisements/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Advertisement(models.Model):
@@ -10,6 +13,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('изображение', upload_to='advertisements')
 
     @admin.display(description='последнее обновление')
     def updated_date(self):
@@ -28,6 +33,12 @@ class Advertisement(models.Model):
             return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
         
         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
+        
+    @admin.display(description='фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html('<img src ="{url}" width="100" height="100">', url=self.image.url)  
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
